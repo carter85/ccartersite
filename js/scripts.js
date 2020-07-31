d5ceca0bad9b2b2a3d4064bd04f1f88a1c47b609
Casey Carter <caseycarter8520@gmail.com>
2020-07-31 16:46:08 -0500

    added chap1 to notes.html, made hide button

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function hideShit() {
+    var x = document.getElementById("chOne");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
