f47d2612f027003348f8f5e0cf693f52187b7d88
Casey Carter <caseycarter8520@gmail.com>
2020-08-02 21:36:47 -0500

    created flash card thing with JS, HTML, and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 function hideShit() {
     var x = document.getElementById("chOne");
     if (x.style.display === "none") {
@@ -7,3 +8,82 @@ function hideShit() {
         x.style.display = "none";
     }
 }
+
+function nextCard() {
+    const randomElement = curd[Math.floor(Math.random() * curd.length)];
+    document.getElementById("topText").innerHTML = randomElement.term;
+    document.getElementById("bottomText").innerHTML = randomElement.def;    
+        }
+        
+        class Card {
+            constructor(term, def) {
+          this.term = term;
+          this.def = def;
+        }
+      }
+
+const card1 = new Card("Script", 'A script is a series of instructions that a computer can follow to achieve a goal. It can be compared to recipes, handbooks, or manuals');
+const card2 = new Card('Vocabulary', 'The words that computers can understand');
+const card3 = new Card('Syntax', 'How words are put together to create instructions a computer can follow');
+const card4 = new Card('Object', 'In computer programming, each physical thing in the world can be represented as an object.');
+const card5 = new Card('Properties', 'Properties describe the characteristics of an object. Each property has a name and a value, and each tell you something about the object.');
+const card6 = new Card('Events', 'An event is something that happens on a computer that the computer can react to, such as a keypress or moving the mouse over an object.');
+const card7 = new Card('Methods','Methods represent things that people need to do with objects. They can retrieve  or update the values of an objects properties. You do not always need to know how a method achieves a task, but you need to know how to ask the question and how to interpret any ansers ig gives you.');
+const card8 = new Card('Document Object Model(DOM)','When a browser creates a model of a web page, it also creates a new object for each element on the page, together these objects are described as the Document Object Model(DOM).');
+const card9 = new Card('Interpreter','The interpreter takes instructions and translates them into instructions the computer can use to achieve the tasks you want it to perform.');
+const card10 = new Card('Interpreted Programming Language','An interpreted programming language, like JS, each line of code is translated one-by-one as the script is run.');
+const card11 = new Card('Seperation of Concerns','When possible, aim to keep the three languages in seperate files, with the HTML page linking to the  CSS and JS files. This is referred to as the seperation of concerns.');
+const card12 = new Card('Content Layer','Where the content of the page lives, the HTML gives the page structure and adds semantics. Stored in .html files');
+const card13 = new Card('Presentation Layer','The CSS enhances the HTML page with rules that state how the HTML content is presented. Stored in .css files.');
+const card14 = new Card('Behavior Layer','Where the behavior of the page can be changed, adding interactivity. Stored in .js files.');
+const card15 = new Card('Progressive Enhancement','Progressive enhancement is the strategy of implementing HTML first, focusing on content, followed by CSS to improve the look of the page, and then JavaScript, adding interactivity.');
+const card16 = new Card('Calling','Programmers refer to using a method of an object as calling.');
+const card17 = new Card('.','The full stop is referred to as the member operator. You access the members of an object using a dot between the object name and the member you want to access.');
+const card18 = new Card('Statements','Each individual instruction or step in a script is known as a statement. Statements should end with a semicolon.');
+const card19 = new Card('Single-Line Comments','In a single-line comment, anything that follows the two forward slashes // on that line will not be processed by the JS interpreter.');
+const card20 = new Card('Multi-line Comments','To write a comment that stretches over multiple lines, start with /* and end with */.');
+const card21 = new Card('Variable','A script will have to temporarily store the bits of information it needs to do its job. It can store this data in variables.');
+const card22 = new Card('var','var is an example of what programmers call a keyword. It is used to declare a statement is a variable.');
+const card23 = new Card('camelCase','camelCase is a method of writing in which the first word is lower case, but each following word has the first letter uppercased..');
+const card24 = new Card('=','The equals sign is an assignment operator, it says that you are going to assign a value to a variable.');
+const card25 = new Card('Numeric Data Type','The  numeric data type handles numbers.');
+const card26 = new Card('String Data Type','The string data type consits of letters and other characters encased in a pair of quotes.');
+const card27 = new Card('Boolean Data Types','Boolean data types can have one of two values: true or false.');
+const card28 = new Card('Arrays',"An array is a special type of variable, it stores a list of values.");
+const card29 = new Card('Array Constructor','A technique for creating an array using the New keyword, the values are then specified in parentheses instead of square brackets, and each value is seperated by a comma.');
+const card30 = new Card('Expressions','An expression evaluates (results in) a single value.');
+const card31 = new Card('Operators','Expressions rely on operators; they allow programmers to create a single value from one or more values.');
+
+const curd = [
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+    card9,
+    card10,
+    card11,
+    card12,
+    card13,
+    card14,
+    card15,
+    card16,
+    card17,
+    card18,
+    card19,
+    card20,
+    card21,
+    card22,
+    card23,
+    card24,
+    card25,
+    card26,
+    card27,
+    card28,
+    card29,
+    card30,
+    card31,
+]
