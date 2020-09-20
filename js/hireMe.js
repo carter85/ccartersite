3bb7bb33c8b824fd20d063b7383b7da461945ef0
Casey Carter <caseycarter8520@gmail.com>
2020-09-20 08:50:24 -0500

    fixed issue with containers not resizing or aligning correctly

--- js/hireMe.js
+++ js/hireMe.js
@@ -8,3 +8,12 @@ function myFunction() {
   }
 }
 
+/* what was I using this script to change? language and tools grid classes? */
+function myFunction() {
+  var x = document.getElementById("languages tools");
+  if (x.className === "offset-by-two three columns knowledge genericContainer") {
+    x.className = "genericContainer";
+  } else {
+    x.className = "offset-by-two three columns knowledge genericContainer";
+  }
+}
